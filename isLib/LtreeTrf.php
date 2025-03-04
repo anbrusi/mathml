6a62aee1b3bdb58689ad3b12564d52a10adfe44c
Andreas Brunnschweiler <anbrusi@bluewin.ch>
2025-03-04 14:26:59 +0100

    Rework of distributive law

--- isLib/LtreeTrf.php
+++ isLib/LtreeTrf.php
@@ -44,6 +44,21 @@ class LtreeTrf {
         return $n;
     }
 
+    private function handleSubtreeOnly(array $node):array {
+        $n = $this->copyNodeHeader($node);
+        if (isset($node['u'])) {
+            // unary node
+            $n['u'] = $this->dist($node['u']);
+        } elseif ($this->isTerminal($node)) {
+            // Do nothing, terminal nodes have no link
+        } else {
+            // binary node
+            $n['l'] = $this->dist($node['l']);
+            $n['r'] = $this->dist($node['r']);
+        }
+        return $n;
+    }
+
     /**
      * We call a tree "handled" if it has no "mult" nodes with "add" subnodes. 
      * dist returns a "handled" node, which is mathematically equivalent to $node.
@@ -61,7 +76,7 @@ class LtreeTrf {
             $l = $this->dist($node['l']);
             $r = $this->dist($node['r']);
             $leftAdd = $this->isAddNode($l);
-            $rightAdd = $this->isAddNode($r);
+            $rightAdd = ($this->isAddNode($r) && $node['tk'] != '/'); // No distribution of divisor
             if ($leftAdd && !$rightAdd) {  // EX: (10-2)?4 $node['tk'] = '?', $l['tk] = '-', $l['l'] = 10, $node['r'] = 4               
                 // $leftProd = 10*4
                 $leftProd = ['tk' => $node['tk'], 'type' => $node['type'], 'restype' => $node['restype']];
@@ -88,18 +103,45 @@ class LtreeTrf {
                 $n = ['tk' => $r['tk'], 'type' => $r['type'], 'restype' => $r['restype']];
                 $n['l'] = $leftProd;
                 $n['r'] = $rightProd;
-            } elseif ($leftAdd && $rightAdd) {
-
+            } elseif ($leftAdd && $rightAdd) { // Ex: // (2+3)*(7-5) = 2*(7-5) + 3*(7-5) = 2*7 - 2*5 + 3*7 - 3*5
+                $p1 = ['tk' => $node['tk'], 'type' => $node['type'], 'restype' => $node['restype']]; // 2*7
+                $p1['l'] = $l['l'];
+                $p1['r'] = $r['l'];
+                $p2 = ['tk' => $node['tk'], 'type' => $node['type'], 'restype' => $node['restype']]; // 2*5
+                $p2['l'] = $l['l'];
+                $p2['r'] = $r['r'];
+                $p3 = ['tk' => $node['tk'], 'type' => $node['type'], 'restype' => $node['restype']]; // 3*7
+                $p3['l'] = $l['r'];
+                $p3['r'] = $r['l'];
+                $p4 = ['tk' => $node['tk'], 'type' => $node['type'], 'restype' => $node['restype']]; // 3*5
+                $p4['l'] = $l['r'];
+                $p4['r'] = $r['r'];
+                $addopl = $l['tk'];
+                $addopr = $r['tk'];
+                // Build left associative "add"
+                // First two products
+                $n1 = ['tk' => $addopr, 'type' => $r['type'], 'restype' => $r['restype']]; // 2*7 - 2*5
+                $n1['l'] = $p1;
+                $n1['r'] = $p2;
+                // Result and third product. Addop is the first one 
+                $n2 = ['tk' => $addopl, 'type' => $r['type'], 'restype' => $r['restype']]; // (2*7 - 2*5) + 3*7
+                $n2['l'] = $n1;
+                $n2['r'] = $p3;
+                // Result and fourth product
+                if ($addopl == $addopr) {
+                    $addop = '+';
+                } else {
+                    $addop = '-';
+                }
+                $n = ['tk' => $addop, 'type' => $r['type'], 'restype' => $r['restype']]; // ((2*7 - 2*5) + 3*7) -3*5
+                $n['l'] = $n2;
+                $n['r'] = $p4;
             } else {
-                $n = ['tk' => $node['tk'], 'type' => $node['type'], 'restype' => $node['restype']];
-                $n['l'] = $this->dist($node['l']);
-                $n['r'] = $this->dist($node['r']);
+                $this->handleSubtreeOnly($node);
             }
         } else {
             // Although $node is no "mult" node, we must still take care of subnodes. So we cannot just return $node
-            $n = ['tk' => $node['tk'], 'type' => $node['type'], 'restype' => $node['restype']];
-            $n['l'] = $this->dist($node['l']);
-            $n['r'] = $this->dist($node['r']);
+            $n = $this->handleSubtreeOnly($node);
         }
         return $n;
     }
